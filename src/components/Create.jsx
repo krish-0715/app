6d69b7bb89e7e7075f191ec2481c7ea360586eff
krish-0715 <muthukrishnanarumugam9@gmail.com>
2023-04-16 20:34:09 +0530

    completed app

---
+++ src/components/Create.jsx
@@ -0,0 +1,90 @@
+import React ,{ useState }from "react";
+import {toast } from "react-toastify"
+import { createContact } from "../db/store"; // {} typed / named imports 
+
+// to read values from inputs in react forms 
+// mutable -> useRef() hook
+// immutable -> state and onchange method 
+
+const getRandom = () => {
+     return Math.round(Math.random() * 10000);  // random id 
+}
+
+function Create (props) {
+
+   // const [state,handler()] = useState(initialValue)
+
+   const [fname,setFName] = useState('')
+   const [femail,setFEmail] = useState('')
+   const [fmobile,setFMobile] = useState('')
+   const [fimage,setFImage] = useState('')
+   const [faddress,setFAddress] = useState('')
+
+   const submitHandler = async (e) => {
+    e.preventDefault(); // to avoid page refresh
+    let data = {
+        id: getRandom(),
+        name: fname,
+        email: femail,
+        mobile: fmobile,
+        image: fimage,
+        address: faddress
+    };
+
+        console.log('new contact =', data);
+        createContact(data);
+        //toast.success('successfully Submitted');
+   }
+
+    return(
+        // how to declare the states 
+        // const [state,handler()] = useState(initialValues)
+        // 
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 text-center">
+                <h3 className="display-4 text-success">Create new</h3>
+                </div>
+            </div>
+
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <div className="card">
+                        <div className="card-body">
+                            <form autoComplete="off" onSubmit={submitHandler}>
+                                <div className="form-group mt-2">
+                                    <label htmlFor ="name">Name</label>
+                                    <input type="text" name="name" value={ fname } onChange={(e) => setFName(e.target.value)}  id="name" className="form-control" required />
+                                </div>
+                                <div className="form-group mt-2">
+                                    <label htmlFor ="email">Email</label>
+                                    <input type="email" name="email" value={ femail } onChange={(e) => setFEmail(e.target.value)} id="email"  className="form-control" required />
+                                </div>
+                                <div className="form-group mt-2">
+                                    <label htmlFor ="Mobile">Mobile</label>
+                                    <input type="number" name="mobile" value={ fmobile } onChange={(e) => setFMobile(e.target.value)}  id="mobile" className="form-control" required />
+                                </div>
+                                <div className="form-group mt-2">
+                                    <label htmlFor ="image">Image</label>
+                                    <input type="url" name="image" value={ fimage } onChange={(e) => setFImage(e.target.value)} id="image"  className="form-control" required />
+                                </div>
+                                <div className="form-group mt-2">
+                                    <label htmlFor ="address">Address</label>
+                                    <textarea type="address" name="address" value={ faddress } onChange={(e) => setFAddress(e.target.value)} id="address" cols="30" rows="5" className="form-control" required></textarea>
+                                </div>
+                                <div className="form-group mt-2">
+                                <input type="submit" value="create" className="btn btn-outline-success" />
+                                </div>
+                                
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+    );
+        
+}
+
+export default Create 
+
---
+++ src/components/Home.jsx
@@ -0,0 +1,35 @@
+import React,{ useState, useEffect } from "react";
+import { readContacts } from '../db/store';
+import AddressCard from "./AddressCard";
+
+function Home(props) {
+    const [contact,setContact] = useState([])
+    // useEffect -> will be called before component render
+    //useEffect(handler,[dependancy])
+    useEffect(() => {
+        console.log('called Useeffect');
+        let data = readContacts();
+        setContact(data);
+    },[])
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12 text-center">
+                    <h3 className="display-3 text-success">Home Page</h3>
+                </div>
+            </div>
+            <div className="row">
+                {
+                    contact.map((item,index) => {
+                        return (
+                            <AddressCard key={index} {...item} />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Home
